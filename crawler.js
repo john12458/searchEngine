a9e1a2092bf149da73aaf4513f5b18dd3a6616f1
john12458 <john210628@gmail.com>
2020-03-12 22:10:04 +0800

    delete other

--- crawler.js
+++
@@ -1,8 +0,0 @@
-var rp = require('request-promise');
-rp('https://www.pixnet.net')
-    .then(function (htmlString) {
-        console.log(htmlString)
-    })
-    .catch(function (err) {
-        // Crawling failed...
-    });
